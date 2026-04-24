50fa3e333cbc3672df74a9a32cefa56ca2bb9d5f
Gabriel Montague <montaguegabe@users.noreply.github.com>
2026-04-24 17:13:47 -0400

    Scaffold initial its-now API package

---
+++ its_now_api/its_now/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ItsNowConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "its_now_api.its_now"
---
+++ its_now_api/its_now/urls.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+# All URLs will be prefixed with /api/
+urlpatterns = []
